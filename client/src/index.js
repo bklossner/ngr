8cd55ffce2a4a9d79f95c3d9993bb8b497ed87af
Bob K <farfisa5@gmail.com>
2018-09-22 02:52:14 -0500

    Importing new index.css file after moving it to /css folder.

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
