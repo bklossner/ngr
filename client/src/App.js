9310224623b8a48176e2c302297bded26417ef00
Bob K <farfisa5@gmail.com>
2018-09-22 02:51:21 -0500

    Importing <Header> component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-//import './App.css';
+import './css/App.css';
+import Header from './components/header';
 
 class App extends React.Component {
   
   render() {
     return (
       <div className="App">
-        <div className="container">
-          <div className="jumbotron">
-            <h1>NGR</h1>
-          </div>
-        </div>
+        <Header />
       </div>
     );
   }
