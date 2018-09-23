126644587dd4acb3c85c3bbb517887bb26d91473
Bob K <farfisa5@gmail.com>
2018-09-23 18:03:05 -0500

    Added routes for all games, single game, and runSQL function.

--- server/routes/games.js
+++ server/routes/games.js
@@ -7,11 +7,44 @@ const express = require('express');
 const router = express.Router();
 
 /**
+ * @description Takes in the sql, request, response, and outputs the sql result in JSON format
+ * @param sql The SQL to run
+ * @param req The request object
+ * @param res The response object
+ */
+function runSQL(sql, req, res) {
+  
+  // Execute query
+  res.locals.connection.query(sql, function (error, results, fields) {
+    if(error) {
+      console.error('error connecting: ' + error.stack);
+      throw error;
+    }
+    res.send(JSON.stringify(results));
+  });
+}
+
+/**
  * @description Return all games from the database
  * @example http://someurl/games
  */
 router.get('/', function (req, res) {
-  res.send("Games router - Not Implemented")
+  
+  /**
+   * @description SQL to return all games in the database
+   * @type {string} allGamesSQL
+   */
+  const allGamesSQL = `
+    select game.id, game.title, genre.genre, developer.developer,
+           rating.rating, release_year.release_year
+    from game, genre, developer, rating, release_year
+    where game.genre_id = genre.id and
+          game.developer_id = developer.id and
+          game.year_id = release_year.id and
+          game.rating_id = rating.id
+    order by game.genre_id`;
+  
+  runSQL(allGamesSQL, req, res);
 });
 
 /**
@@ -19,7 +52,24 @@ router.get('/', function (req, res) {
  * @example http://someurl/games/[gameId]
  */
 router.get('/:gameId', function (req, res) {
-  res.send("Game ID router - Not Implemented. Game Id: " + req.params.gameId);
+  // res.send("Game ID router - Not Implemented. Game Id: " + req.params.gameId);
+  
+  /**
+   * @description SQL to return all games in the database
+   * @type {string} allGamesSQL
+   */
+  const allGamesSQL = `
+    select game.id, game.title, genre.genre, developer.developer,
+           rating.rating, release_year.release_year
+    from game, genre, developer, rating, release_year
+    where game.genre_id = genre.id and
+          game.developer_id = developer.id and
+          game.year_id = release_year.id and
+          game.rating_id = rating.id and
+          game.id = ${req.params.gameId}
+    order by game.genre_id`;
+  
+  runSQL(allGamesSQL, req, res);
 });
 
 /**
