69b48706f7f839f5d011de6bd6b533aba620d390
Bob K <farfisa5@gmail.com>
2018-09-23 16:25:49 -0500

    Importing dropdown component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './css/App.css';
 import Header from './components/header';
+import SortByDropDown from './components/SortByDropDown';
 
 class App extends React.Component {
   
@@ -8,6 +9,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
+        <SortByDropDown/>
       </div>
     );
   }
