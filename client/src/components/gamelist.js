48f8766929b7e406de0fc0483301d1e35bd4954c
Bob K <farfisa5@gmail.com>
2018-09-24 01:10:35 -0500

    Initial commit.

---
+++ client/src/components/gamelist.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Game from './game';
+
+class GameList extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      games: []
+    }
+  }
+  
+  componentDidMount() {
+    
+    let self = this;
+    
+    fetch('/games', {
+      method: 'GET'})
+      .then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        self.setState({games: data});
+      })
+      .catch(err => {
+        console.log('caught it!', err);
+      })
+  }
+  
+  render() {
+    
+    let cols = 3;
+    let count = 1;
+    
+    return (
+      <div className="container">
+        
+        <div className="row">
+        {this.state.games.map(game =>
+          <div className="col-sm-4">
+            <Game key={game.id} dev={game.developer} genre={game.genre} id={game.id} rating={game.rating} title={game.title} year={game.release_year} />
+          </div>
+        )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GameList;
