3e5a12666142fe6307c01e50ff02886e6f5cb7c9
Bob K <farfisa5@gmail.com>
2018-09-25 17:17:11 -0500

    Initial adding of file to project.

---
+++ client/src/components/gradient-overlay.js
@@ -0,0 +1,33 @@
+/**
+ * @class GradientOverlay
+ * @description A gradient overlay that will darken an image, from top (light) to bottom (dark)
+ * @author Bob Klossner
+ */
+
+import React from 'react';
+
+class GradientOverlay extends React.Component {
+  
+  /**
+   * @description Defines the styles (CSS)
+   * @type {{content: string, position: string, top: number, left: number, backgroundImage: string, opacity: number, width: string, height: string}}
+   */
+  overlayStyles = {
+    content : '',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7))',
+    opacity: 0.6,
+    width: '100%',
+    height: '100%'
+  };
+  
+  render(){
+    return (
+      <div className="gradient-overlay" style={this.overlayStyles} />
+    );
+  }
+}
+
+export default GradientOverlay
