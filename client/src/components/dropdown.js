8152f441fc165b3474f47865be1fbfbf2f0435ea
Bob K <farfisa5@gmail.com>
2018-09-25 17:16:07 -0500

    Renaming file.

---
+++ client/src/components/dropdown.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Dropdown extends React.Component {
+
+  render() {
+    return (
+      <div className="container dropdown-container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="btn-group">
+              <button type="button" className="btn btn-outline-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Sort by
+              </button>
+              <div className="dropdown-menu">
+                <a className="dropdown-item" href="#">Name</a>
+                <a className="dropdown-item" href="#">Genre</a>
+                <a className="dropdown-item" href="#">Year</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dropdown;
