7571b363225318ae299d9cf11428f4858a629da9
Bob K <farfisa5@gmail.com>
2018-11-29 17:42:35 -0600

    Implementing @reach/router capabilities.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import './css/App.css';
 
-import Home from './components/Home';
+import { Router } from '@reach/router';
 
-import Router from '@reach/router';
+import NavSticky from './components/NavSticky';
+import Header from './components/Header';
+import Home from './components/Home';
+import GameInfo from './components/GameInfo';
 
 class App extends React.Component {
   
   render() {
     return (
       <div className="App">
-        <Home />
+        
+        {/* Sticky navbar */}
+        <NavSticky />
+        
+        {/* Top Hero (banner) images */}
+        <Header />
+        
+        {/* Specify the different routes (pages) */}
+        <Router>
+          {/* Root */}
+          <Home path="/" />
+          
+          {/* Unique Game Information page */}
+          <GameInfo path="/gameinfo" />
+        </Router>
+        
       </div>
     );
   }
