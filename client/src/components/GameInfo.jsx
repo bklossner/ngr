12fc38a75fc757d39884adfca5377c69636a2c81
Bob K <farfisa5@gmail.com>
2018-11-29 16:08:34 -0600

    Initial commit.

---
+++ client/src/components/GameInfo.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class GameInfo extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    
+    return(
+      <div>
+        <h2>Game Info</h2>
+      </div>
+    )
+  }
+}
+
+export default GameInfo;
