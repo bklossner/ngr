93a8022125dd468634badedbfb60fedc8dfb416e
Bob K <farfisa5@gmail.com>
2018-11-29 17:24:04 -0600

    Creating a Home component that will reference the landing page. Needed for routing implementation.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import './css/App.css';
 
-import NavSticky from './components/NavSticky';
-import Header from './components/Header';
-import SortByDropDown from './components/SortByDropDown';
-import GameList from './components/GameList';
+import Home from './components/Home';
+
+import Router from '@reach/router';
 
 class App extends React.Component {
   
   render() {
     return (
       <div className="App">
-        <NavSticky />
-        <Header />
-        <SortByDropDown />
-        <GameList />
+        <Home />
       </div>
     );
   }
