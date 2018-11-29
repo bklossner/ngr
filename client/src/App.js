478f86d24d53d5734ba82954aff09964330795aa
Bob K <farfisa5@gmail.com>
2018-11-29 16:02:14 -0600

    Adding NavSticky component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './css/App.css';
 
-
+import NavSticky from './components/NavSticky';
 import Header from './components/Header';
 import SortByDropDown from './components/SortByDropDown';
 import GameList from './components/GameList';
@@ -11,6 +11,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <NavSticky />
         <Header />
         <SortByDropDown />
         <GameList />
