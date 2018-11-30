ff6ff1346bd68ca6931b3fac1295b0c66ab505e1
Bob K <farfisa5@gmail.com>
2018-11-29 23:30:50 -0600

    Adding fetch functionality, retrieving the game info based on id.

--- client/src/components/GameInfo.jsx
+++ client/src/components/GameInfo.jsx
@@ -6,9 +6,49 @@ import React from 'react';
  */
 class GameInfo extends React.Component {
   
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      developer: '',
+      genre: '',
+      id: 0,
+      rating: 0,
+      release_year: 1992,
+      review: '',
+      title: ''
+    }
+  }
+  
   componentDidMount() {
+    // Get the gameId from the game clicked in the thumbnail list
     let gameId = this.props.id;
-    console.log("gameId", gameId);
+    
+    // Use this id to query the game's unique information
+    let self = this;
+  
+    fetch(`/games/${gameId}`, {
+      method: 'GET'})
+      .then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+      })
+      .then(function(data) {
+        self.setState({
+          developer: data[0].developer,
+          genre: data[0].genre,
+          id: data[0].id,
+          rating: data[0].rating,
+          release_year: data[0].release_year,
+          review: data[0].review,
+          title: data[0].title
+        });
+      })
+      .catch(err => {
+        console.log('caught it!', err);
+      });
   }
   
   render() {
@@ -17,7 +57,12 @@ class GameInfo extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col">
-            <h1>{this.props.id}</h1>
+            <h1>{this.state.title}</h1>
+            <p>{this.state.release_year}</p>
+            <p>{this.state.genre}</p>
+            <p>{this.state.developer}</p>
+            <p>Rating: {this.state.rating}/10</p>
+            <p>{this.state.review}</p>
           </div>
         </div>
       </div>
