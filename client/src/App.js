ed9728f140ef1d53e99bc95d1ba9b6d3dfefc314
Bob K <farfisa5@gmail.com>
2018-12-04 12:47:54 -0600

    Adding sticky logic.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './css/App.css';
-
+import $ from 'jquery';
 import { Router } from '@reach/router';
 
 import NavSticky from './components/NavSticky';
@@ -10,12 +10,41 @@ import GameInfo from './components/GameInfo';
 
 class App extends React.Component {
   
+  constructor(props) {
+    super(props);
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+  
+  componentDidMount(){
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  
+  componentWillUnmount(){
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+  
+  handleScroll(){
+    
+    let dropdownLocation = $('.dropdown-container').offset().top;
+  
+    let yScrollPos = window.pageYOffset;
+  
+    if(yScrollPos > dropdownLocation) {
+      console.log("Show sticky");
+      $('.navbar').slideDown();
+    }
+    else {
+      console.log("Hide sticky");
+      $('.navbar').fadeOut();
+    }
+  }
+  
   render() {
     return (
       <div className="App">
         
         {/* Sticky navbar */}
-        {/*<NavSticky />*/}
+        <NavSticky />
         
         {/* Top Hero (banner) images */}
         <Header />
