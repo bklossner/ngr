06faf5b0ab018378405b63568abc91f6ff1a58bd
Bob K <farfisa5@gmail.com>
2018-12-04 19:01:24 -0600

    Adding scrolling logic.

--- client/src/App.js
+++ client/src/App.js
@@ -10,35 +10,6 @@ import GameInfo from './components/GameInfo';
 
 class App extends React.Component {
   
-  constructor(props) {
-    super(props);
-    this.handleScroll = this.handleScroll.bind(this);
-  }
-  
-  componentDidMount(){
-    window.addEventListener('scroll', this.handleScroll);
-  }
-  
-  componentWillUnmount(){
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-  
-  handleScroll(){
-    
-    let dropdownLocation = $('.dropdown-container').offset().top;
-  
-    let yScrollPos = window.pageYOffset;
-  
-    if(yScrollPos > dropdownLocation) {
-      console.log("Show sticky");
-      $('.navbar').slideDown();
-    }
-    else {
-      console.log("Hide sticky");
-      $('.navbar').fadeOut();
-    }
-  }
-  
   render() {
     return (
       <div className="App">
