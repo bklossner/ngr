dc04ef753243978f36d470143ac4c7014f8a9e2c
Bob K <farfisa5@gmail.com>
2018-12-04 19:02:40 -0600

    Adding sticky navbar scrolling functionality.

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,17 +2,52 @@ import React from 'react';
 
 import SortByDropDown from './SortByDropDown';
 import GameList from './GameList';
+import $ from 'jquery';
+
+let sorters = [{ property: 'title', direction: 'ASC' }];
 
 /**
  * @desc The landing page
  */
 class Home extends React.Component {
   
+  constructor(props) {
+    super(props);
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+  
+  componentDidMount(){
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  
+  componentWillUnmount(){
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+  
+  handleScroll(){
+  
+    /**
+     * The Y-value of the .dropdown-container element
+     * @type {number|jQuery}
+     */
+    let dropdownLocation = $('.dropdown-container').offset().top;
+  
+    /**
+     * The current value of the Y axis
+     * @type {number}
+     */
+    let yScrollPos = window.pageYOffset;
+    
+    // If the current value of the Y axis is below the dropdownLoction, show the sticky navbar
+    // If it's above the dropdownLocation, hide the sticky navbar
+    yScrollPos > dropdownLocation ? $('.navbar').slideDown() : $('.navbar').fadeOut();
+  }
+  
   render() {
     return(
       <div>
         <SortByDropDown />
-        <GameList />
+        <GameList sorters={sorters} />
       </div>
     )
   }
