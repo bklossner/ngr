c8dd9372655eb2e7a6472baf8521b6e973d9ac1e
Bob K <farfisa5@gmail.com>
2018-12-04 19:03:24 -0600

    Adding initial sort event handlers.

--- client/src/components/SortByDropDown.jsx
+++ client/src/components/SortByDropDown.jsx
@@ -1,25 +1,91 @@
 import React from 'react';
+import GameList from './GameList';
 
 class SortByDropDown extends React.Component {
 
+  initialSort = 'title';
+  
   constructor(props) {
     super(props);
+    
     this.state = {
-      currentSort : "Name"
+      sortBy : this.initialSort,
+      sortByDisplay : this.capitalizeFirstLetter(this.initialSort)
     };
   }
+  
+  /**
+   * @desc Utility function to capitalize the first letter in a string. Used in displaying
+   * the dropdown in this case
+   * @param string
+   * @return {string}
+   */
+  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  // TODO: There has to be a better way to get the selected sort value instead of having three unique functions.
+  
+  /**
+   * @desc Function to sort by game name.
+   */
+  sortByName = () => {
+    this.setState({
+      sortBy: 'title',
+      sortByDisplay: this.capitalizeFirstLetter('title')
+    }, () => {
+      this.sort();
+    });
+  };
+  
+  /**
+   * @desc Function to sort by game release_year
+   */
+  sortByYear = () => {
+    this.setState({
+      sortBy: 'release_year',
+      sortByDisplay: this.capitalizeFirstLetter('year')
+    }, () => {
+      this.sort();
+    });
+  };
+  
+  /**
+   * @desc Function to sort by game genre
+   */
+  sortByGenre = () => {
+    this.setState({
+      sortBy: 'genre',
+      sortByDisplay: this.capitalizeFirstLetter('genre')
+    }, () => {
+      this.sort();
+    });
+  };
+  
+  /**
+   * @desc Function to run the sort method and display the newly reordered list
+   */
+  sort = () => {
+    console.log("sort()", this.state.sortBy);
+    let sorters = [{ property: this.state.sortBy, direction: 'ASC' }];
+    return <GameList sorters={sorters} />
+  };
   
   render() {
     return (
       <div className="container">
-        <div className="btn-group">
-          <button type="button" className="btn btn-outline-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Sort by {this.state.currentSort}
-          </button>
-          <div className="dropdown-menu">
-            <a className="dropdown-item" href="#">Name</a>
-            <a className="dropdown-item" href="#">Year</a>
-            <a className="dropdown-item" href="#">Genre</a>
+        <div className="row">
+          <div className="dropdown-container">
+            <div className="btn-group">
+              <button type="button" className="btn btn-outline-dark dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Sort by {this.state.sortByDisplay}
+              </button>
+              <div className="dropdown-menu">
+                <div className="dropdown-item" id="title" onClick={this.sortByName}>Title</div>
+                <div className="dropdown-item" id="release_year" onClick={this.sortByYear}>Year</div>
+                <div className="dropdown-item" id="genre" onClick={this.sortByGenre}>Genre</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
