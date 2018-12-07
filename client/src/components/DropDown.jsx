bc5f653c747c82eef9732c3550512fa66b24ecde
Bob K <farfisa5@gmail.com>
2018-12-07 00:31:50 -0600

    Replacing three different sort functions with just one sort().

--- client/src/components/DropDown.jsx
+++ client/src/components/DropDown.jsx
@@ -2,75 +2,27 @@ import React from 'react';
 
 class DropDown extends React.Component {
   
-  initialSort = 'title';
-  
   constructor(props) {
     super(props);
     
     this.state = {
-      sortBy : this.initialSort,
-      sortByDisplay : this.capitalizeFirstLetter(this.initialSort)
+      sortBy: 'title',
+      displayVal: 'Title'
     };
     
-    this.sortByName = this.sortByName.bind(this);
-    this.sortByYear = this.sortByYear.bind(this);
-    this.sortByGenre = this.sortByGenre.bind(this);
+    this.sort = this.sort.bind(this);
   }
   
-  /**
-   * @desc Utility function to capitalize the first letter in a string. Used in displaying
-   * the dropdown in this case
-   * @param string
-   * @return {string}
-   */
-  capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  sort(condition, display) {
+    this.setState({
+      sortBy: condition,
+      displayVal: display
+      }, () => {
+        alert(`Sort by ${this.state.sortBy}`);
+        // Call the sort function
+      }
+    );
   }
-  
-  // TODO: There has to be a better way to get the selected sort value instead of having three unique functions.
-  
-  /**
-   * @desc Function to sort by game name.
-   */
-  sortByName() {
-    this.setState({
-      sortBy: 'title',
-      sortByDisplay: this.capitalizeFirstLetter('title')
-    }, () => {
-      this.sort();
-    });
-  };
-  
-  /**
-   * @desc Function to sort by game release_year
-   */
-  sortByYear() {
-    this.setState({
-      sortBy: 'release_year',
-      sortByDisplay: this.capitalizeFirstLetter('year')
-    }, () => {
-      this.sort();
-    });
-  };
-  
-  /**
-   * @desc Function to sort by game genre
-   */
-  sortByGenre() {
-    this.setState({
-      sortBy: 'genre',
-      sortByDisplay: this.capitalizeFirstLetter('genre')
-    }, () => {
-      this.sort();
-    });
-  };
-  
-  /**
-   * @desc Function to run the sort method and display the newly reordered list
-   */
-  sort() {
-    let sorters = [{ property: this.state.sortBy, direction: 'ASC' }];
-  };
   
   render() {
     return (
@@ -79,12 +31,13 @@ class DropDown extends React.Component {
           <div className="dropdown-container">
             <div className="btn-group">
               <button type="button" className="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Sort by {this.state.sortByDisplay}
+                Sort by {this.state.displayVal}
               </button>
               <div className="dropdown-menu">
-                <div className="dropdown-item text-center" id="title" onClick={this.sortByName}>Title</div>
-                <div className="dropdown-item text-center" id="release_year" onClick={this.sortByYear}>Year</div>
-                <div className="dropdown-item text-center" id="genre" onClick={this.sortByGenre}>Genre</div>
+  
+                <div className="dropdown-item text-center" id="title" onClick={this.sort.bind(this, 'title', 'Title')}>Title</div>
+                <div className="dropdown-item text-center" id="release_year" onClick={this.sort.bind(this, 'release_year', 'Year')}>Year</div>
+                <div className="dropdown-item text-center" id="genre" onClick={this.sort.bind(this, 'genre', 'Genre')}>Genre</div>
               </div>
             </div>
           </div>
