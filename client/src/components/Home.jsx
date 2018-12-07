9a79eb9a6dd4e67891de1916a79701eed5b19b74
Bob K <farfisa5@gmail.com>
2018-12-07 01:50:56 -0600

    Adding handleScroll back.

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,11 +2,47 @@ import React from 'react';
 
 import DropDown from './DropDown';
 import GameList from './GameList';
+import $ from 'jquery';
 
 /**
  * @desc The landing page
  */
 class Home extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+  
+  componentDidMount(){
+    // Add event listener for scrolling the viewport (necessary for the sticky navbar logic)
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  
+  componentWillUnmount(){
+    // Remove the viewport-scrolling event listener
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+  
+  handleScroll(){
+  
+    /**
+     * The Y-value of the .dropdown-container element
+     * @type {number|jQuery}
+     */
+    let dropdownLocation = $('.dropdown-home').offset().top;
+  
+    /**
+     * The current value of the Y axis
+     * @type {number}
+     */
+    let yScrollPos = window.pageYOffset;
+    
+    // If the current value of the Y axis is below the dropdownLoction, show the sticky navbar
+    // If it's above the dropdownLocation, hide the sticky navbar
+    yScrollPos > dropdownLocation ? $('.navbar').slideDown() : $('.navbar').fadeOut();
+  }
   
   render() {
     return(
