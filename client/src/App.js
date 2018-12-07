a9d6551b24e41a33b7297e65458cafd145ec8a51
Bob K <farfisa5@gmail.com>
2018-12-06 23:35:14 -0600

    Moving scroll listener from <Home> to <App>

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './css/App.css';
 import { Router } from '@reach/router';
+import $ from 'jquery';
 
 import NavSticky from './components/NavSticky';
 import Header from './components/Header';
@@ -8,6 +9,41 @@ import Home from './components/Home';
 import GameInfo from './components/GameInfo';
 
 class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+  
+  componentDidMount(){
+    // Add event listener for scrolling the viewport (necessary for the sticky navbar logic)
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  
+  componentWillUnmount(){
+    // Remove the viewport-scrolling event listener
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+  
+  handleScroll(){
+    
+    /**
+     * The Y-value of the .dropdown-container element
+     * @type {number|jQuery}
+     */
+    let dropdownLocation = $('.dropdown-home').offset().top;
+    
+    /**
+     * The current value of the Y axis
+     * @type {number}
+     */
+    let yScrollPos = window.pageYOffset;
+    
+    // If the current value of the Y axis is below the dropdownLoction, show the sticky navbar
+    // If it's above the dropdownLocation, hide the sticky navbar
+    yScrollPos > dropdownLocation ? $('.navbar').slideDown() : $('.navbar').fadeOut();
+  }
   
   render() {
     return (
